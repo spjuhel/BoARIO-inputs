ce84706de7b768aba84ccf4b37205605a311711d
Samuel Juhel <samuel.juhel@zaclys.net>
2022-05-10 09:49:23 +0200

    better aggregation script and files

--- scripts/aggreg_exio3_sectors.py
+++ scripts/aggreg_exio3_sectors.py
@@ -13,7 +13,6 @@ import numpy as np
 parser = argparse.ArgumentParser(description='Aggregate an EXIOBASE3 MRIO table in less sectors')
 parser.add_argument('exio_path', type=str, help='The str path to the exio3 (zip file or already pre-treated pkl file)')
 parser.add_argument('aggreg_path', type=str, help='The str path to the ods aggregation matrix file')
-parser.add_argument('sector_names_json_path', type=str, help='The str path to the json file with the new names of the sectors')
 parser.add_argument('original_mrio_params', type=str, help='A path to the json file of the original mrio parameters file', nargs='?', default=None)
 parser.add_argument('-o', "--output", type=str, help='The path to save the pickled mrio to', nargs='?', default='./mrio_dump')
 parser.add_argument('-po', "--params_output", type=str, help='The path to save the new params to')
@@ -56,9 +55,15 @@ def lexico_reindex(mrio: pym.IOSystem) -> pym.IOSystem:
 
     return mrio
 
-def new_params_from_old(new_sectors,old_mrio_params,sec_agg_matrix):
-    sec_agg_matrix.index = new_sectors
-    sector_mapping = {k : [kk for kk in v.keys() if v[kk] == 1] for k,v in sec_agg_matrix.T.to_dict().items()}
+def new_params_from_old(old_mrio_params, sec_agg_vec, sec_agg_new_names):
+    dico = sec_agg_vec[['sector','new_sectors']].set_index("sector").to_dict()['new_sectors']
+    sector_mapping = {}
+    for old, new in dico.items():
+        if new in sector_mapping.keys():
+            sector_mapping[new].append(old)
+        else:
+            sector_mapping[new] = [old]
+    #sector_mapping = {v : [kk for kk in v.keys() if v[kk] == 1] for k,v in sec_agg_vec.T.to_dict().items()}
     new_mrio_params = old_mrio_params.copy()
     new_mrio_params['capital_ratio_dict'] = {k:0 for k in sector_mapping.keys()}
     for k in new_mrio_params['capital_ratio_dict'].keys():
@@ -73,13 +78,11 @@ def new_params_from_old(new_sectors,old_mrio_params,sec_agg_matrix):
 
     return new_mrio_params
 
-def aggreg(exio_path, sector_aggregator_path, new_sectors_name_path, old_mrio_params_path, save_path=None, mrio_params_save_path=None):
+def aggreg(exio_path, sector_aggregator_path, old_mrio_params_path, save_path=None, mrio_params_save_path=None):
     scriptLogger.info("Loading region aggregator")
     scriptLogger.info("Make sure you use the same python environment as the one loading the pickle file (especial pymrio and pandas version !)")
     scriptLogger.info("Your current environment is: {}".format(os.environ['CONDA_PREFIX']))
     params=False
-    with pathlib.Path(new_sectors_name_path).open('r') as f:
-        a = json.load(f)
 
     if old_mrio_params_path is not None:
         old_mrio_params_path = pathlib.Path(old_mrio_params_path)
@@ -104,8 +107,9 @@ def aggreg(exio_path, sector_aggregator_path, new_sectors_name_path, old_mrio_pa
     else:
         raise TypeError("File type ({}) not recognize for the script (must be zip or pkl) : {}".format(exio_path.suffix,exio_path.resolve()))
 
-    old_sectors = exio3.get_sectors()
-    sec_agg_matrix = pd.read_excel(sector_aggregator_path, sheet_name="input", engine="odf", header=None, names=old_sectors)
+    sec_agg_vec = pd.read_excel(sector_aggregator_path, sheet_name="aggreg_input", engine="odf")
+    sec_agg_newnames = pd.read_excel(sector_aggregator_path, sheet_name="name_input", engine="odf", index_col=0, squeeze=True)
+    sec_agg_vec = sec_agg_vec.sort_values(by="sector")
 
     # gain some diskspace and RAM by removing unused attributes
     attr = ['Z', 'Y', 'x', 'A', 'L', 'unit', 'population', 'meta', '__non_agg_attributes__', '__coefficients__', '__basic__']
@@ -121,13 +125,14 @@ def aggreg(exio_path, sector_aggregator_path, new_sectors_name_path, old_mrio_pa
     exio3 = lexico_reindex(exio3)
     scriptLogger.info("Done")
     scriptLogger.info("Reading aggregation matrix from sheet 'input' in file {}".format(pathlib.Path(sector_aggregator_path).absolute()))
-    scriptLogger.info("Aggregating from {} to {} sectors".format(len(exio3.get_sectors()), sec_agg_matrix.shape[0])) #type:ignore
-    exio3.aggregate(sector_agg=sec_agg_matrix.to_numpy())
-    scriptLogger.info("Done")
-    scriptLogger.info("Renaming sectors from {}".format(pathlib.Path(new_sectors_name_path).absolute()))
-    exio3.rename_sectors(a)
+    scriptLogger.info("Aggregating from {} to {} sectors".format(len(exio3.get_sectors()), len(sec_agg_vec.group.unique()))) #type:ignore
+    sec_agg_vec['new_sectors'] = sec_agg_vec.group.map(sec_agg_newnames.to_dict())
+    exio3.aggregate(sector_agg=sec_agg_vec.new_sectors.values)
+    #scriptLogger.info("Done")
+    #scriptLogger.info("Renaming sectors from {}".format(pathlib.Path(new_sectors_name_path).absolute()))
+    #exio3.rename_sectors(a)
     exio3.calc_all()
-    scriptLogger.info("Done")
+    #scriptLogger.info("Done")
     name = save_path
     scriptLogger.info("Saving to {}".format(pathlib.Path(name).absolute()))
     exio3 = lexico_reindex(exio3)
@@ -136,7 +141,7 @@ def aggreg(exio_path, sector_aggregator_path, new_sectors_name_path, old_mrio_pa
 
     if params:
         scriptLogger.info("Generation new mrio params from {}".format(pathlib.Path(old_mrio_params_path).absolute()))
-        new_params = new_params_from_old(exio3.get_sectors(),old_mrio_params,sec_agg_matrix) #type:ignore
+        new_params = new_params_from_old(old_mrio_params, sec_agg_vec, sec_agg_newnames)
         scriptLogger.info("Saving these new params to {}".format(pathlib.Path(mrio_params_save_path).absolute()))
         with pathlib.Path(mrio_params_save_path).open('w') as f:
             json.dump(new_params, f, indent=4)
@@ -145,4 +150,4 @@ def aggreg(exio_path, sector_aggregator_path, new_sectors_name_path, old_mrio_pa
 if __name__ == '__main__':
     args = parser.parse_args()
     name = pathlib.Path(args.exio_path).stem
-    aggreg(args.exio_path, args.aggreg_path, args.sector_names_json_path, old_mrio_params_path=args.original_mrio_params, save_path=args.output, mrio_params_save_path=args.params_output)
+    aggreg(args.exio_path, args.aggreg_path, old_mrio_params_path=args.original_mrio_params, save_path=args.output, mrio_params_save_path=args.params_output)
