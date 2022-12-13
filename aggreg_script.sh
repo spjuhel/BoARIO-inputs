4c14695b93e0edfe04a109f0cce5a7d099dcb2c6
Samuel Juhel <samuel.juhel@zaclys.net>
2022-12-13 09:33:47 +0100

    added 2035-50

--- aggreg_script.sh
+++ aggreg_script.sh
@@ -10,13 +10,13 @@ showHelp() {
     cat << EOF
 Usage: $0 --type=<exp_type> --output=<output_dir> [ --phase=<phase> --psi=<psi> --custom-run=<dir> --semester=<max_sem> ]
 
--h,                    --help                       Display help
--t [hist|proj],        --type [hist|proj]           Set the experience type
--o <dir>,              --output <dir>               Set the output directory to <dir>
--P [1-6],              --phase [1-6]                Run only phase <phase>
--Y,                    --psi [0.0,1.0]              Run only for this psi parameter
--C,                    --custom-run <dir>           Run for a non default simulation
--S [1-8],              --semester <max_sem>         Run with semester disagregation
+-h,                    --help                              Display help
+-t [hist|proj],        --type [hist|proj1|proj2]           Set the experience type
+-o <dir>,              --output <dir>                      Set the output directory to <dir>
+-P [1-6],              --phase [1-6]                       Run only phase <phase>
+-Y,                    --psi [0.0,1.0]                     Run only for this psi parameter
+-C,                    --custom-run <dir>                  Run for a non default simulation
+-S [1-8],              --semester <max_sem>                Run with semester disagregation
 
 
 EOF
@@ -82,11 +82,17 @@ then
     exp_path="${exp_dir}experience-flood-dottori-1970-2005";
     flood_base_path="/data/sjuhel/BoARIO-inputs/source-data/full_floodbase_1970_2005.parquet"
     rep_events="/data/sjuhel/BoARIO-inputs/source-data/representative_events_1970_2005.parquet"
-elif [[ "$exp_type" == "proj" ]]
+elif [[ "$exp_type" == "proj1" ]]
 then
     exp_path="${exp_dir}experience-flood-dottori-2016-2035"
     flood_base_path="/data/sjuhel/BoARIO-inputs/source-data/full_floodbase_2016_2035.parquet"
     rep_events="/data/sjuhel/BoARIO-inputs/source-data/representative_events_2016_2035.parquet"
+fi
+elif [[ "$exp_type" == "proj2" ]]
+then
+    exp_path="${exp_dir}experience-flood-dottori-2035-2050"
+    flood_base_path="/data/sjuhel/BoARIO-inputs/source-data/full_floodbase_2035_2050.parquet"
+    rep_events="/data/sjuhel/BoARIO-inputs/source-data/representative_events_2035_2050.parquet"
 fi
 
 if [[ $custom_run -eq 1 ]]
